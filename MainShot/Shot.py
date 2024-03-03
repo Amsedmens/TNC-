7383f71790ddb0291f349df64ebae7c6aa3e70c6
Amsedmens <68100004+Amsedmens@users.noreply.github.com>
2024-03-03 16:54:39 +0500

    Create Shot.py

---
+++ MainShot/Shot.py
@@ -0,0 +1,14 @@
+# Класс для пули
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((5, 5))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed = 10
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()
